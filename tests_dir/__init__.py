d810d5106588020a4010997e139b58164c5d7cc4
Ilan Brandvain <ilanb@mobileye.com>
2020-10-05 20:27:17 +0300

    init tests

---
+++ tests_dir/__init__.py
@@ -0,0 +1,9 @@
+"""
+a directory for all of the tests for the app
+we are keeping outside of the app itseld for so we can easily deploy without the tests
+each module in the app should have one test file
+all functions should have at least one test. More are better.
+integration tests can be added as necessary
+to run the tests run this from the top level of the repo:
+python manage.py test tests_dir
+"""

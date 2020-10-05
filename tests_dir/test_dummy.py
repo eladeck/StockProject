971edbcfed21110a971ee431ec435e074ca531d6
Ilan Brandvain <ilanb@mobileye.com>
2020-10-05 20:27:38 +0300

    dummy tests to use as an example

---
+++ tests_dir/test_dummy.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from django.test import TestCase  # since we are not using the database, we will just use the builtin unittest.TestCase
+from unittest import TestCase
+
+class testNothingAtAll(TestCase):
+
+    def test_demo_equal(self):
+        a_number: int = 1
+        self.assertEqual(a_number, a_number)
+
+    def test_demo_false(self):
+        this_is_false: List = []  # an empty list is False
+        self.assertFalse(this_is_false)
+
+
+

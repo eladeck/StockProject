6b57509b8bd813759258a3f56906e0e9b6a782a0
Mohammad-Mouadi <mohammad.moaddi@gmail.com>
2020-10-20 17:17:11 +0300

    removed extra stuff2

--- myapp/stock_api.py
+++ myapp/stock_api.py
@@ -50,12 +50,6 @@ def get_stock_historic_prices(symbol, time_range='1m'):
 
 
 
-def get_stock_earning(symbol, last):
-    return _request_data('/stable/stock/{symbol}/earnings/{last}'.format(symbol=symbol, last=last),
-                         filter='actualEPS,consensusEPS,announceTime,numberOfEstimates,EPSSurpriseDollar,yearAgoChangePercent,currency',
-                         additional_parameters={'period': 'qurter'})
-
-
 def get_all_stocks():
     return _request_data('/beta/ref-data/symbols')
 

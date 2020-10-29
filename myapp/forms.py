d8f0cd5524295ab0b6979ab63ae02744a89d73ef
Musab Jolani <34215119+musabjolani@users.noreply.github.com>
2020-10-29 14:32:45 +0200

    changes_in_forms

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,13 +1,22 @@
 from django import forms
-from myapp.models import User
+from myapp.models import UserProfile
+from django.contrib.auth.models import User
 
 
-class MyAccountForm(forms.ModelForm):
-    first_name = forms.CharField(max_length=50, label='*First Name', required=True)
-    last_name = forms.CharField(max_length=50, label='*Last Name', required=True)
-    email = forms.EmailField(label='*Email', required=True)
-    balance = forms.DecimalField(disabled=True)
+class UserForm(forms.ModelForm):
+    username = forms.CharField(max_length=150, label='*User Name', required=True)
+    first_name = forms.CharField(max_length=150, label='*First Name', required=True)
+    last_name = forms.CharField(max_length=150, label='*Last Name', required=True)
+    email = forms.EmailField(max_length=254, label='*Email', required=True)
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+
+class UserProfileForm(forms.ModelForm):
+    balance = forms.DecimalField(disabled=True)
+
+    class Meta:
+        model = UserProfile
+        fields = ['balance']

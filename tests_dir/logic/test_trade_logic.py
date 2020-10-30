64ba59934a542f1db69e1ee627d676e39ab42641
Mohammad-Mouadi <mohammad.moaddi@gmail.com>
2020-10-30 19:58:27 +0300

    Added unit testing for Trade Logic

---
+++ tests_dir/logic/test_trade_logic.py
@@ -0,0 +1,67 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from myapp import models
+from myapp.logic import trade_logic
+import requests
+
+
+class TestTradeLogic(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        login = self.client.login(username='testuser', password='12345')
+
+
+    def test_parse_input(self):
+        params = {
+            'number_of_stocks': 10,
+            'stock_selector': 'A'
+        }
+        stock_info = trade_logic.parse_input(params)
+        self.assertIsNotNone(stock_info['number_of_stocks'])
+        self.assertIsNotNone(stock_info['total_price'])
+
+    def test_parse_input_invalid_data(self):
+        params = {
+            'number_of_stocks': 'a',
+            'stock_selector': 'A'
+        }
+        stock_info = trade_logic.parse_input(params)
+        self.assertIsNone(stock_info)
+
+    def test_create_transaction(self):
+        stock_info = {
+            'symbol':'A',
+            'number_of_stocks': 10,
+            'total_price': 30.0
+        }
+
+        trade_logic.create_transaction(user=self.user,stock_info=stock_info)
+
+        does_exist = models.Transaction.objects.filter(user=self.user).exists()
+        self.assertTrue(does_exist)
+
+    def test_create_profile(self):
+        trade_logic.create_profile(self.user)
+        does_exist = models.UserProfile.objects.filter(user=self.user).exists()
+        self.assertTrue(does_exist)
+
+    def test_get_number_of_stocks(self):
+        stock_info = {
+            'symbol': 'A',
+            'number_of_stocks': 10,
+            'total_price': 30.0
+        }
+        trade_logic.create_transaction(user=self.user, stock_info=stock_info)
+
+        stock_info = {
+            'symbol': 'A',
+            'number_of_stocks': -5,
+            'total_price': 30.0
+        }
+        trade_logic.create_transaction(user=self.user, stock_info=stock_info)
+
+        number_of_stocks = trade_logic.get_number_of_stocks(self.user,'A')
+        self.assertEqual(number_of_stocks,5)
+
+

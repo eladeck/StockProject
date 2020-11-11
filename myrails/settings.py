856844e8e0f4154a3a396f8a1b9d82572cd56c54
Alaa Yahia <alaa.yahia.1995@gmail.com>
2020-11-11 19:42:16 +0200

    first commit

--- myrails/settings.py
+++ myrails/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -124,6 +124,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'myapp/static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/'
 
@@ -134,3 +136,7 @@ CACHES = {
         'LOCATION': '/var/tmp/django_cache',
     }
 }
+
+
+# Activate Django.Heroku
+django_heroku.settings(locals())

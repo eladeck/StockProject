83a319d7934853f0afb91c86d0898477b3a67734
Alaa Yahia <alaa.yahia.1995@gmail.com>
2020-11-13 01:25:13 +0200

    Update settings.py

--- myrails/settings.py
+++ myrails/settings.py
@@ -119,13 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'myapp/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/'

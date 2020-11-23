6ee65461b4fdb3c63d8d07295ebdb0dbd0555fd9
Mohammad-Mouadi <mohammad.moaddi@gmail.com>
2020-11-23 22:21:53 +0200

    test

--- myrails/settings.py
+++ myrails/settings.py
@@ -35,6 +35,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -110,8 +111,17 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 if os.getcwd() == '/app':
-    DEBUG = False
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.9/howto/static-files/
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
     django_heroku.settings(locals())
 else:
     DEBUG = True
